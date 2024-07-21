97ce9aeb45631426bf99f5f884f7fa5f5d4c0eb5
takusan23 <takumu1200@gmail.com>
2024-07-22 02:51:07 +0900

    スプラッシュスクリーン

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.datastore.preferences)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/io/github/takusan23/komadroid/MainActivity.kt
+++ app/src/main/java/io/github/takusan23/komadroid/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.github.takusan23.komadroid.ui.screen.MainScreen
 import io.github.takusan23.komadroid.ui.theme.KomaDroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
