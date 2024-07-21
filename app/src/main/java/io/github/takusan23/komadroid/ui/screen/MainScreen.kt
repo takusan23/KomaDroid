2917d277d065fdd4b94a90fd6ca94e9c4ad419c8
takusan23 <takumu1200@gmail.com>
2024-07-22 00:28:44 +0900

    aosp も

--- app/src/main/java/io/github/takusan23/komadroid/ui/screen/MainScreen.kt
+++ app/src/main/java/io/github/takusan23/komadroid/ui/screen/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import io.github.takusan23.komadroid.PermissionTool
+import io.github.takusan23.komadroid.ui.screen.setting.LicenseScreen
 
 enum class MainScreenNavigation(val path: String) {
     PermissionScreen("permission"),
--- app/src/main/java/io/github/takusan23/komadroid/ui/screen/setting/LicenseScreen.kt
+++ app/src/main/java/io/github/takusan23/komadroid/ui/screen/setting/LicenseScreen.kt
@@ -1,4 +1,4 @@
-package io.github.takusan23.komadroid.ui.screen
+package io.github.takusan23.komadroid.ui.screen.setting
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,6 +21,24 @@ import androidx.compose.ui.unit.sp
 private data class LicenseData(
     val name: String,
     val license: String,
+)
+
+private val aosp = LicenseData(
+    name = "Android Open Source Project",
+    license = """
+        Copyright (C) 2013 The Android Open Source Project
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+             http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+    """.trimIndent()
 )
 
 private val kotlinCoroutine = LicenseData(
@@ -61,6 +79,7 @@ private val materialIcons = LicenseData(
 
 /** ライセンス一覧 */
 private val LicenseList = listOf(
+    aosp,
     kotlinCoroutine,
     materialIcons
 )
