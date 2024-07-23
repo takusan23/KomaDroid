c740fd85e69f2868632ebf56057de42bb1819818
takusan23 <takumu1200@gmail.com>
2024-07-24 02:22:14 +0900

    審査リベンジ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.takusan23.komadroid"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
